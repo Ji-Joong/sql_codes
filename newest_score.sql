f3a6fc006b92b52a817707403976162a2bbc0cc8
Jijoong Kim <jj.kim@riiid.co>
2015-12-01 16:59:37 +0900

    Update newest_score.sql

--- newest_score.sql
+++ newest_score.sql
@@ -1,6 +1,9 @@
 select user_id, count(passed) as score
 from quiz_items
-where passed is true and quiz_item_type = 'newest'
+where passed is true and quiz_id in (select id
+                                          from quizzes
+                                         where finished is true
+                                           and quiz_type = 'newest')
 group by user_id
 order by score;
---유저별로 진단테스트 30문제 중 몇문제 맞췄는지
+--진단테스트 마친유저별로 진단테스트 30문제 중 몇문제 맞췄는지
