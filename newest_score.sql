f4da13e6dd750adadffd59ac5394d344186d2f2d
Jijoong Kim <jj.kim@riiid.co>
2015-12-09 18:51:59 +0900

    Update newest_score.sql

--- newest_score.sql
+++ newest_score.sql
@@ -6,14 +6,14 @@ where passed is true and quiz_id in (select id
                                            and quiz_type = 'newest')
 group by user_id
 order by score;
---진단테스트 마친유저별로 진단테스트 30문제 중 몇문제 맞췄는지
+--수정한 버전
 select user_id, count(passed) as correct
 from quiz_items
 where quiz_id in (select id from quizzes where quiz_type = 'newest' and finished is true)
   AND user_id in (select user_id from users where admin is false)
+  --어드민 계정을 빼고 계산.
   and passed is true
   and created_at >= '2015-10-23 03:00'
+--10월 23일 12:00에 클베 런칭했다고 치고 이렇게 계산.
 group by user_id
 order by correct;
---어드민 계정을 빼고 계산.
---근데 진단테스트 완료 인원보다 높게 나옴. 이거 왜이러는걸까요?
