43366dd13bc73646f1a242ae52a90e1010b4d6ed
Jijoong Kim <jj.kim@riiid.co>
2015-12-17 12:11:51 +0900

    Update cycle 정책 변경 이후 user 관찰.sql

--- cycle 정책 변경 이후 user 관찰.sql
+++ cycle 정책 변경 이후 user 관찰.sql
@@ -3,3 +3,19 @@ from quizzes
 where user_id in (select id from users where created_at >= '2015-12-03 09:00' and admin is false )
 group by user_id
 order by user_id;
+
+--order 수정, 신규 가입 유저만 해당
+
+select user_id, count(finished_at)
+from quizzes
+where user_id in (select id from users where created_at >= '2015-12-03 09:00' and admin is false )
+group by user_id
+order by count;
+
+--기존 가입유저중 모의고사 넘은 사람
+select user_id, count(finished_at)
+from quizzes
+where user_id in (select user_id from quizzes where finished_at >= '2015-12-03 09:00' and quiz_type = 'diagnosis')
+  and user_id in (select id from users where created_at <= '2015-12-03 09:00' and admin is false)
+group by user_id
+order by count;
